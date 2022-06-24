f83b08e0394c1b17746c36a0aecfbd3cbdb15f53
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-06-24 19:59:18 +0900

    [Add]State作成開始

---
+++ Assets/State/State.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+/// <summary>
+/// クラス説明
+/// </summary>
+public class State : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
