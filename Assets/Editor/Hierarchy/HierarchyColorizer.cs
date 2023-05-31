ce22f7fd45eae5b29fec463fe63d033ca03dc84b
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-05-31 16:10:27 +0900

    [add]Hierarchyを一行おきに色を変えて見やすくするエディタ拡張を作製

---
+++ Assets/Editor/Hierarchy/HierarchyColorizer.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Hierarchyを一行おきに色を変えて見やすくするエディタ拡張
+/// </summary>
+[InitializeOnLoad]
+internal class HierarchyColorizer
+{
+    private const int RowHeight = 16;
+    private const int OffsetY = -4;
+    private static readonly Color Color = new Color(0, 0, 0, 0.3f);
+
+    static HierarchyColorizer()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += ColorizeHierarchyItem;
+    }
+
+    private static void ColorizeHierarchyItem(int instanceID, Rect rect)
+    {
+        int index = (int)(rect.y + OffsetY) / RowHeight;
+
+        if (index % 2 == 0)
+        {
+            return;
+        }
+
+        float xMax = rect.xMax;
+
+        rect.x = GetIndentation();
+        rect.xMax = xMax + GetExtraWidth();
+
+        EditorGUI.DrawRect(rect, Color);
+    }
+
+    private static float GetIndentation()
+    {
+        return 32; 
+    }
+
+    private static float GetExtraWidth()
+    {
+        return 16; 
+    }
+}
